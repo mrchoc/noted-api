1178bf35629db26329e3f7805e64a8972c1fafea
mrchoc <ryanloh28@yahoo.com.sg>
2021-10-22 00:12:54 +0800

    Added options handler

---
+++ middleware/options.js
@@ -0,0 +1,7 @@
+module.exports = function (req, res, next) {
+  if (req.method == "OPTIONS") {
+    return res.sendStatus(200);
+  } else {
+    next(ex);
+  }
+};
--- startup/routes.js
+++ startup/routes.js
@@ -2,10 +2,12 @@ const express = require("express");
 const notes = require("../routes/notes");
 const users = require("../routes/users");
 const auth = require("../routes/auth");
+const options = require("../middleware/options");
 
 module.exports = function (app) {
   app.use(express.json());
   app.use("/api/notes", notes);
   app.use("/api/users", users);
   app.use("/api/auth", auth);
+  app.use(options);
 };
